b8f013bfb58acb02df78bbdf10e4b0f581fbc297
Florian Marsch <florianmarsch@florians-macbook-pro.local>
2018-03-17 11:58:36 +0100

    Initial Commit

---
+++ FOM-Prozedurale Programmierung/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FOM-Prozedurale Programmierung
+//
+//  Created by Florian Marsch on 17.03.18.
+//  Copyright © 2018 Florian Marsch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
